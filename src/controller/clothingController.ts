5d9f4c2f0be1f92e6b9056aeb5bc653f60ac2231
onetrillion <tradgardsmastaren@protonmail.com>
2023-06-30 00:46:52 +0200

    add routes and clothing controller

---
+++ src/controller/clothingController.ts
@@ -0,0 +1,24 @@
+import { IClothingPayload } from "src/interfaces/IClothingPayload";
+import { Get, Route, Tags, Post, Delete, Body, Path } from "tsoa";
+import { Clothing } from "../entity/clothing";
+import { getClothing, addClothing, removeClothing } from "src/repository/clothingRepository";
+
+@Route("clothing")
+@Tags("Clothing")
+export default class ClothingController {
+
+  @Get("/api/v1/clothing")
+  public async getClothing(): Promise<Array<Clothing>> {
+    return getClothing();
+  }
+
+  @Post("/api/v1/clothing")
+  public async addClothing(@Body() body: IClothingPayload): Promise<Clothing> {
+    return addClothing(body);
+  }
+
+  @Delete("/api/v1/clothing/:id")
+  public async removeClothing(@Path() id: string): Promise<Clothing | null> {
+    return removeClothing(Number(id));
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,30 @@
 import express from "express";
+import ClothingController from "src/controller/clothingController";
 
 const router = express.Router();
 
 router.get("/health", (_req, res) => {
     return res.send("ok");
 });
+
+router.get("/api/v1/clothing", async (_req, res) => {
+    const controller = new ClothingController();
+    const clothing = await controller.getClothing();
+    return res.send(clothing);
+})
+
+router.post("/api/v1/clothing", async (req, res) => {
+    const controller = new ClothingController();
+    const clothing = await controller.addClothing(req.body);
+    return res.send(clothing);
+})
+
+router.delete("/api/v1/clothing/:id", async (req, res) => {
+    const controller = new ClothingController();
+    const clothing = await controller.removeClothing(req.params.id);
+    if (!clothing) res.status(404).send({message: `no clothing with id: ${req.params.id} found`})
+    return res.send(clothing);
+})
+
 
 export default router;
